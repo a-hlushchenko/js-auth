4d8b4791fbe44fb2eb04d6e96fe58718def6c9e5
AntonHlushchenko <glantoshka@gmail.com>
2023-09-13 19:28:32 +0300

    upd route

--- src/route/index.js
+++ src/route/index.js
@@ -4,9 +4,28 @@ const router = express.Router()
 router.get('/', function (req, res) {
   res.render('signup', {
     name: 'signup',
-    component: [''],
-    title: 'Головна',
-    data: {},
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
+
+    title: 'Реєстрація',
+    data: {
+      role: [
+        { value: User.USER_ROLE.USER, text: 'Користувач' },
+        {
+          value: User.USER_ROLE.ADMIN,
+          text: 'Адміністратор',
+        },
+        {
+          value: User.USER_ROLE.DEVELOPER,
+          text: 'Розробник',
+        },
+      ],
+    },
   })
 })
 
